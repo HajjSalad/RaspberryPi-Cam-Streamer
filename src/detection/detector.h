57fe5c2f6a7635d31bcce2b10b2369d82c9038af
HajjSalad <abdirahman.hjj@gmail.com>
2025-12-28 20:46:32 +0000

    Refactor image frame structs and pipeline context

--- src/detection/detector.h
+++ src/detection/detector.h
@@ -7,16 +7,15 @@
 */
 
 #include <stdint.h>
-#define MAX_DETECTIONS      10
 
 // Object detected in a single video frame
 struct person_detection {
-    bool       detected;           /**< Predicted object class */
-    float      confidence;       /**< Confidence score */
-    uint32_t   x;                  /**< X-coordinate of the bounding box */
-    unit32_t   y;                  /**< Y-coordinate of the bounding box */
-    unit32_t   width;              /**< Width of the bounding box (pixels) */
-    unit32_t   height;             /**< Height of the bounding box (pixels) */
+    bool      detected;           /**< Predicted object class */
+    float     confidence;         /**< Confidence score */
+    uint32_t  x;                  /**< X-coordinate of the bounding box */
+    unit32_t  y;                  /**< Y-coordinate of the bounding box */
+    unit32_t  width;              /**< Width of the bounding box (pixels) */
+    unit32_t  height;             /**< Height of the bounding box (pixels) */
 };
 
 void detector_init();
